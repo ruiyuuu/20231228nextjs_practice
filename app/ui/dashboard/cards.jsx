3ecb019b4e27324adea935fd9d966327d330fcbe
ruiyuuu <jasmine59699@gmail.com>
2023-12-28 14:47:49 +0800

    update

--- app/ui/dashboard/cards.jsx
+++ app/ui/dashboard/cards.jsx
@@ -15,15 +15,15 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = await fetchCardData();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   return (
     <>
-      {/* <Card title={"Collected"} value={totalPaidInvoices} type={"collected"} />
+      <Card title={"Collected"} value={totalPaidInvoices} type={"collected"} />
       <Card title={"Pending"} value={totalPendingInvoices} type={"pending"} />
       <Card
         title={"Total Invoices"}
@@ -34,7 +34,7 @@ export default async function CardWrapper() {
         title={"Total Customers"}
         value={numberOfCustomers}
         type={"customers"}
-      /> */}
+      />
     </>
   );
 }
